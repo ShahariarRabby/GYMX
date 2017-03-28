1c10c9c93f8161d71b3dd3a26f8c01b9d363ff7f
shahariarrabby <shahariarrabby@gmail.com>
2017-03-29 03:02:50 +0600

    All working with profile

--- resources/views/layouts/users.blade.php
+++ resources/views/layouts/users.blade.php
@@ -10,7 +10,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!--        <link rel="stylesheet" href="..js/Countdown/demo/css/demo.css">-->
-    <link href="{{asset('css/userprofile.css')}}" rel="stylesheet">
+    {{--<link href="{{asset('css/userprofile.css')}}" rel="stylesheet">--}}
+    {!! Html::style( asset('css/userprofile.css')) !!}
+
+
 
 </head>
 
