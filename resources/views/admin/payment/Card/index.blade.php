b982a13d1b00fd0a6cf9c2b4fb5193ba5cc4ed39
shahariarrabby <shahariarrabby@gmail.com>
2017-04-06 20:51:27 +0600

    gg

--- resources/views/admin/payment/Card/index.blade.php
+++ resources/views/admin/payment/Card/index.blade.php
@@ -1,33 +1,31 @@
-@extends('layouts.admin')
-@section('header')
-    Cards
-@endsection
-@section('content')
+{{----}}@extends('layouts.admin')--}}
+{{--@section('header')--}}
+{{--Cards--}}
+{{--@endsection--}}
+{{--@section('content')--}}
 
-    <table class="table table-hover table-responsive">
-        <thead>
-        <tr>
-            <th>Card No</th>
-            <th>Amount</th>
-            <th>Status</th>
-            <th>Created At</th>
+{{--<table class="table table-hover table-responsive">--}}
+{{--<thead>--}}
+{{--<tr>--}}
+{{--<th>Card No</th>--}}
+{{--<th>Amount</th>--}}
+{{--<th>Status</th>--}}
+{{--<th>Created At</th>--}}
 
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($card as $card)
-            <tr>
+{{--</tr>--}}
+{{--</thead>--}}
+{{--<tbody>--}}
+{{--@foreach($card as $card)--}}
+{{--<tr>--}}
 
-                <td><a href="{{route('card.edit', [$card->id])}}">{{$card->id}}</a></td>
-                <td>{{$card->amount}}</td>
-                <td> {{$card->status}}</td>
-                <td>{{$card->updated_at->diffForHumans()}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
-
+{{--<td><a href="{{route('card.edit', [$card->id])}}">{{$card->id}}</a></td>--}}
+{{--<td>{{$card->amount}}</td>--}}
+{{--<td> {{$card->status}}</td>--}}
+{{--<td>{{$card->updated_at->diffForHumans()}}</td>--}}
+{{--</tr>--}}
+{{--@endforeach--}}
+{{--</tbody>--}}
+{{--</table>--}}
 
 
 
@@ -38,4 +36,10 @@
 
 
 
-@endsection
+
+
+{{--@endsection
+
+
+
+222222
