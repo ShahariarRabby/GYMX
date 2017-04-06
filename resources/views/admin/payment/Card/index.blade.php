ca728f1a2b28133759c0b9d2f555cc946faa96a4
shahariarrabby <shahariarrabby@gmail.com>
2017-04-06 22:31:57 +0600

    g

--- resources/views/admin/payment/Card/index.blade.php
+++ resources/views/admin/payment/Card/index.blade.php
@@ -1,62 +1,27 @@
-{{--@extends('layouts.admin')--}}
-{{--@section('header')--}}
-    {{--bKash--}}
-{{--@endsection--}}
-{{--@section('content')--}}
-
-    {{--<table class="table table-hover table-responsive">--}}
-        {{--<thead>--}}
-        {{--<tr>--}}
-            {{--<th>Card No</th>--}}
-            {{--<th>Amount</th>--}}
-            {{--<th>Status</th>--}}
-            {{--<th>Created At</th>--}}
-
-        {{--</tr>--}}
-        {{--</thead>--}}
-        {{--<tbody>--}}
-        {{--@foreach($card as $card)--}}
-            {{--<tr>--}}
-
-                {{--<td><a href="{{route('card.edit', [$card->id])}}">{{$card->id}}</a></td>--}}
-                {{--<td>{{$card->amount}}</td>--}}
-                {{--<td> {{$card->status}}</td>--}}
-                {{--<td>{{$card->updated_at->diffForHumans()}}</td>--}}
-            {{--</tr>--}}
-        {{--@endforeach--}}
-        {{--</tbody>--}}
-    {{--</table>--}}
-
-
 @extends('layouts.admin')
 @section('header')
-    <i class="fa fa-money" aria-hidden="true"></i>  Payments
+    bKash
 @endsection
 @section('content')
 
     <table class="table table-hover table-responsive">
         <thead>
         <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Credit</th>
-            <th>Debit</th>
-            <th>Balance</th>
-            <th>Type</th>
-            <th>Time</th>
+            <th>Card No</th>
+            <th>Amount</th>
+            <th>Status</th>
+            <th>Created At</th>
 
         </tr>
         </thead>
         <tbody>
-        @foreach($payment as $payment)
+        @foreach($card as $card)
             <tr>
-                <td>{{$payment->id}}</td>
-                <td><a href="{{route('payment.edit', [$payment->id])}}">{{$payment->user->name}}</a></td>
-                <td>{{$payment->Credit}}</td>
-                <td>{{$payment->Debit}}</td>
-                <td> {{$payment->Balance}}</td>
-                <td>{{$payment->Type}}</td>
-                <td>{{$payment->updated_at->diffForHumans()}}</td>
+
+                <td><a href="{{route('card.edit', [$card->id])}}">{{$card->id}}</a></td>
+                <td>{{$card->amount}}</td>
+                <td> {{$card->status}}</td>
+                <td>{{$card->updated_at->diffForHumans()}}</td>
             </tr>
         @endforeach
         </tbody>
@@ -74,13 +39,3 @@
 
 
 @endsection
-
-
-
-
-
-
-
-
-
-{{--@endsection--}}
