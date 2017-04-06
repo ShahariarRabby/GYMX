30870a9c51d94721686396c774afd33b0fddfd6b
shahariarrabby <shahariarrabby@gmail.com>
2017-04-06 21:20:47 +0600

    g

--- app/Http/Controllers/AdminCardControler.php
+++ app/Http/Controllers/AdminCardControler.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Bkash;
 use App\card;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,7 @@ class AdminCardControler extends Controller
     {
         //
 
-        $card = Card::all();
+        $card = Bkash::all();
         return $card;
         return view('admin.payment.card.index',compact('card'));
     }
@@ -26,70 +27,70 @@ class AdminCardControler extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        return view('admin.payment.card.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      Card::create($request->all());
-      return redirect('admin/card');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\card  $card
-     * @return \Illuminate\Http\Response
-     */
-    public function show(card $card)
-    {
-        return redirect('admin/card');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\card  $card
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(card $card)
-    {
-       $cards = Card::findOrFail($card);
-       return view('admin.payment.card.edit',compact('cards'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\card  $card
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, card $card)
-    {
-        //
-        Card::findOrFail($card)->update($request->all());
-        return redirect('admin/card');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\card  $card
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(card $card)
-    {
-        //
-        Card::findOrFail($card)->delete();
-        return redirect('admin/card');
-    }
+//    public function create()
+//    {
+//        return view('admin.payment.card.create');
+//    }
+//
+//    /**
+//     * Store a newly created resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function store(Request $request)
+//    {
+//      Card::create($request->all());
+//      return redirect('admin/card');
+//    }
+//
+//    /**
+//     * Display the specified resource.
+//     *
+//     * @param  \App\card  $card
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function show(card $card)
+//    {
+//        return redirect('admin/card');
+//    }
+//
+//    /**
+//     * Show the form for editing the specified resource.
+//     *
+//     * @param  \App\card  $card
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function edit(card $card)
+//    {
+//       $cards = Card::findOrFail($card);
+//       return view('admin.payment.card.edit',compact('cards'));
+//    }
+//
+//    /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @param  \App\card  $card
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function update(Request $request, card $card)
+//    {
+//        //
+//        Card::findOrFail($card)->update($request->all());
+//        return redirect('admin/card');
+//    }
+//
+//    /**
+//     * Remove the specified resource from storage.
+//     *
+//     * @param  \App\card  $card
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function destroy(card $card)
+//    {
+//        //
+//        Card::findOrFail($card)->delete();
+//        return redirect('admin/card');
+//    }
 }
