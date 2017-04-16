8facd6bd6acc3458d1362585899c132b6cfab249
shahariarrabby <shahariarrabby@gmail.com>
2017-04-16 21:22:15 +0600

    admin chat

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -236,10 +236,9 @@
                         {{--</form>--}}
 
 
-                        <li><a href="{{url('/')}}" data-toggle="tooltip" data-placement="bottom" title="Visit Site" target="_blank"><i class="material-icons">fui-user</i></a></li>
-                        <li><a href="{{url('/profile')}}" data-toggle="tooltip" data-placement="bottom" title="Visit User" target="_blank" style="font-size: larger"><i class="fa fa-user"></i></a></li>
-                        {{--<li><a href="{{url('/profile')}}" data-toggle="tooltip" data-placement="bottom" title="Visit User" target="_blank" style="font-size: larger"><i class="fa fa-user" aria-hidden="true"></i>--}}
-                                {{--</a></li>--}}
+                        <li><a href="{{url('/')}}" data-toggle="tooltip" data-placement="bottom" title="Visit Site" target="_blank"><i class="material-icons">home</i></a></li>
+                        <li><a href="{{url('/profile')}}" data-toggle="tooltip" data-placement="bottom" title="Visit User" target="_blank" style="font-size: larger"><i class="fa fa-user" aria-hidden="true"></i>
+                                </a></li>
                         {{--<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Notification"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span><span class="navbar-new">5</span></a></li>--}}
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="material-icons">more_vert</i></a>
