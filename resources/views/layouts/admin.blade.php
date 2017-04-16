86a1dd3cf0a325d8d2ca39bb707cf4c3c45406ef
shahariarrabby <shahariarrabby@gmail.com>
2017-04-16 21:46:12 +0600

    admin chat

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -237,8 +237,8 @@
 
 
                         <li><a href="{{url('/')}}" data-toggle="tooltip" data-placement="bottom" title="Visit Site" target="_blank"><i class="material-icons">home</i></a></li>
-                        {{--<li><a href="{{url('/')}}" data-toggle="tooltip" data-placement="bottom" title="Visit Site" target="_blank"><i class="material-icons"><i style="font-size: larger" class="fa fa-user" aria-hidden="true"></i></a></li>--}}
-                        <li><a href="{{url('/profile')}}" data-toggle="tooltip" data-placement="bottom" title="Visit User" target="_blank" ><i style="font-size: medium" class="fa fa-user" aria-hidden="true"></i>
+                        <li><a href="{{url('/')}}" data-toggle="tooltip" data-placement="bottom" title="Visit Site" target="_blank"><i class="material-icons">perm_identity</i></a></li>
+                        {{--<li><a href="{{url('/profile')}}" data-toggle="tooltip" data-placement="bottom" title="Visit User" target="_blank" ><i style="font-size: medium" class="fa fa-user" aria-hidden="true"></i>--}}
                                 </a></li>
                         {{--<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Notification"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span><span class="navbar-new">5</span></a></li>--}}
                         <li class="dropdown">
